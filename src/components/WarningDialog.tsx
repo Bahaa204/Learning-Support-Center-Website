a6f127224d1c431fbb9021797f01de2097e64e78
Bahaa Rawass <brawass6@gmail.com>
2026-08-27 22:45:07 +0300

    autofocused the close button on warning dialog and some code formatting

--- src/components/WarningDialog.tsx
+++ src/components/WarningDialog.tsx
@@ -17,6 +17,7 @@ import {
 } from "@headlessui/react";
 import { AlertTriangleIcon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 type WarningDialogProps = {
   IsOpen: boolean;
@@ -34,26 +35,26 @@ export default function WarningDialog({
   return (
     <Dialog
       open={IsOpen}
-      as='div'
-      className='relative z-9999999 focus:outline-none'
+      as="div"
+      className="relative z-9999999 focus:outline-none"
       onClose={onClose}
     >
-      <div className='fixed inset-0 z-9999999 w-screen overflow-y-auto'>
-        <div className='flex min-h-full items-center justify-center p-4'>
-          <DialogBackdrop className='fixed inset-0 bg-black/15' />
+      <div className="fixed inset-0 z-9999999 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4">
+          <DialogBackdrop className="fixed inset-0 bg-black/15" />
           <DialogPanel
             transition
-            className='w-full max-w-lg rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0'
+            className="w-full max-w-lg rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
           >
-            <Card className='ring-0!'>
+            <Card className="ring-0! outline-0! shadow-none!">
               <CardHeader>
-                <CardTitle className='text-3xl'>
-                  <DialogTitle className='flex justify-center items-center gap-2 text-yellow-400'>
+                <CardTitle className="text-3xl">
+                  <DialogTitle className="flex justify-center items-center gap-2 text-yellow-400">
                     <AlertTriangleIcon size={40} /> Still in Development
                   </DialogTitle>
                 </CardTitle>
               </CardHeader>
-              <CardContent className='text-[16px]'>
+              <CardContent className="text-[16px]">
                 This site is currently under active development and will
                 continue to be expanded and improved over time. You may
                 encounter bugs, unexpected behavior, or features that are still
@@ -62,8 +63,8 @@ export default function WarningDialog({
                 <br />
                 If you notice an issue, please report it on the{" "}
                 <Link
-                  to='/report'
-                  className='text-primary underline font-semibold'
+                  to="/report"
+                  className="text-primary underline font-semibold"
                   onClick={onClose}
                 >
                   Report
@@ -73,8 +74,8 @@ export default function WarningDialog({
                 <br />
                 You can also share your suggestions or feedback through the{" "}
                 <Link
-                  to='/feedback'
-                  className='text-primary underline font-semibold'
+                  to="/feedback"
+                  className="text-primary underline font-semibold"
                   onClick={onClose}
                 >
                   Feedback
@@ -85,9 +86,13 @@ export default function WarningDialog({
                 Thank you for your patience and for helping us make the site
                 better.
               </CardContent>
-              <CardFooter className='bg-transparent'>
-                <CardAction className='size-full'>
-                  <Button onClick={onClose} className='size-full py-2 text-lg'>
+              <CardFooter className="bg-transparent">
+                <CardAction className="size-full">
+                  <Button
+                    autoFocus
+                    onClick={onClose}
+                    className="size-full py-2 text-lg"
+                  >
                     Close
                   </Button>
                 </CardAction>
